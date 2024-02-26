667a29fb89b6fd95edccf2e3b88cdf07c6dae078
minh166 <156414238+minh166@users.noreply.github.com>
2024-02-26 13:58:59 +0700

    Add files via upload

---
+++ AutomobileLibrary/DataAccess/BaseDAL.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace AutomobileLibrary.DataAccess
+{
+    public class BaseDAL
+    {
+        public StockDataProvider dataProvider { get; set; } = null;
+        public SqlConnection connection = null;
+
+        public BaseDAL()
+        {
+            var connectionString = GetConnectionString();
+            dataProvider = new StockDataProvider(connectionString);
+        }
+
+        public string GetConnectionString()
+        {
+            string connectionString;
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            connectionString = config["ConnectionString:MyStockDB"];
+            return connectionString;
+        }
+
+        public void CloseConnection() => dataProvider.CloseConnection(connection);
+    }//end class
+}//end namespace
